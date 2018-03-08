3c7fdd2bdce904b6c4a317d5731795c8d949ab97
Martynas Bagdonas <martbgd@gmail.com>
2018-03-08 10:31:04 +0200

    Upload to reports/

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ router.post('/recognize', async function (ctx) {
 
 router.post('/report', async function (ctx) {
 	let res = await s3Client.upload({
-		Key: 'report_' + (new Date().toISOString()) + '_' + ctx.ip + '.json',
+		Key: 'reports/' + (new Date().toISOString()) + '_' + ctx.ip + '.json',
 		Body: JSON.stringify(ctx.request.body),
 	}).promise();
 	ctx.body = {};
