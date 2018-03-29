9ad75c947f40d3bb25d3906f09cc365fce402d4b
Martynas Bagdonas <martbgd@gmail.com>
2018-03-29 12:56:14 +0300

    Fix test

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # recognizer-server
 
+[![Build Status](https://travis-ci.org/zotero/recognizer-server.svg?branch=master)](https://travis-ci.org/zotero/recognizer-server)
+
 Extracts metadata from PDFs.
 
 ```
--- test/pdftest.js
+++ test/pdftest.js
@@ -2,56 +2,60 @@ const fs = require('fs');
 const request = require('request');
 const jsonDiff = require('json-diff');
 const config = require('config');
+const expect = require('chai').expect;
 
-function query(data) {
-	return new Promise(function (resolve, reject) {
-		request({
-			url: 'http://localhost:' + config.get('port') + '/recognize',
-			method: 'POST',
-			json: data,
-		}, function (err, res) {
-			if (err || res.statusCode !== 200) return reject(err);
-			resolve(res.body);
-		});
-	});
-}
-
-async function main() {
-	let ret = 0;
-	let file;
-	
-	let dataPath = __dirname + '/pdftest-data/';
-	if (!fs.existsSync(dataPath)) {
-		console.log('pdftest-data does not exist');
-		process.exit(1);
-	}
-	
-	let files = fs.readdirSync(dataPath);
-	files.sort();
-	
-	while (file = files.shift()) {
-		if (file.slice(-7) !== 'in.json') continue;
-		let id = file.slice(0, 3);
-		console.log(id);
+describe('Recognizer Server', function () {
+	it('should pass PDF tests', async function () {
+		this.timeout(1000 * 60 * 5);
 		
-		let inPath = dataPath + id + '-in.json';
-		let outPath = dataPath + id + '-out.json';
-		
-		let inJson = fs.readFileSync(inPath, 'utf8');
-		inJson = JSON.parse(inJson);
-		let res = await query(inJson);
-		
-		delete res.timeMs;
-		
-		let outJson = fs.readFileSync(outPath, 'utf8');
-		outJson = JSON.parse(outJson);
-		
-		if (jsonDiff.diff(outJson, res)) {
-			console.log(jsonDiff.diffString(outJson, res));
-			ret = 1;
+		function query(data) {
+			return new Promise(function (resolve, reject) {
+				request({
+					url: 'http://localhost:' + config.get('port') + '/recognize',
+					method: 'POST',
+					json: data,
+				}, function (err, res) {
+					if (err || res.statusCode !== 200) return reject(err);
+					resolve(res.body);
+				});
+			});
 		}
-	}
-	process.exit(ret);
-}
-
-main();
+		
+		await require('../server')();
+		
+		let ret = 0;
+		let file;
+		
+		let dataPath = __dirname + '/pdftest-data/';
+		
+		let files = fs.readdirSync(dataPath);
+		files.sort();
+		
+		let failed = 0;
+		
+		while (file = files.shift()) {
+			if (file.slice(-7) !== 'in.json') continue;
+			let id = file.slice(0, 3);
+			console.log(id);
+			
+			let inPath = dataPath + id + '-in.json';
+			let outPath = dataPath + id + '-out.json';
+			
+			let inJson = fs.readFileSync(inPath, 'utf8');
+			inJson = JSON.parse(inJson);
+			let res = await query(inJson);
+			
+			delete res.timeMs;
+			
+			let outJson = fs.readFileSync(outPath, 'utf8');
+			outJson = JSON.parse(outJson);
+			
+			if (jsonDiff.diff(outJson, res)) {
+				console.log(jsonDiff.diffString(outJson, res));
+				failed++;
+			}
+		}
+		
+		expect(failed).to.equal(0);
+	});
+});
